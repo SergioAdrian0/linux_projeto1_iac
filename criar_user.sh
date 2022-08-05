2c21b481bc684db52f8b75311a31dd8ff5e03923
SergioAdrian0 <sergioadrianobc@gmail.com>
2022-08-05 16:02:51 +0000

    Arquivos IaC v.1

---
+++ criar_user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Criando usuários do sistema"
+
+
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m  
+
+
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m  
+
+
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m 
+
+
+useradd guest13 -c "Usuário convidado" -s /bin/bash -m 
+
+
+echo "Finalizado!!!"
+
+
